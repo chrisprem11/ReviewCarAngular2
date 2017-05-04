bc793523f7d1df95a446c8656037c3ba6773b1c8
Premnath Christopher <premnath.christopher@urssystems.com>
2017-05-04 17:43:52 +0530

    Added reviews

--- src/app/component/reviewComponent/review.component.ts
+++ src/app/component/reviewComponent/review.component.ts
@@ -1,10 +1,31 @@
-import {Component} from '@angular/core';
+import {Component,OnInit} from '@angular/core';
+import {ReviewService} from './reviewService/reviewService';
+import {Review} from './../../class/review';
+import {Observable} from 'rxjs/Observable';
+import {Car} from './../../class/car';
+import {CarService} from './../carComponent/carService/carService';
+
 
 @Component({
   selector : 'review-component',
-  templateUrl:'./reviewTemplate/reviews.html'
+  templateUrl:'./reviewTemplate/reviews.html',
+  providers : [ReviewService]
 })
-
 export class ReviewComponent {
+  allReviews : Observable<Review[]>;
+  allCars : Observable<Car[]>;
 
+  constructor(private reviewService : ReviewService,private carService: CarService){}
+
+  getAllReviews(){
+    this.allReviews = this.reviewService.getAllReviews();
+  }
+  getMyCars(){
+    this.allCars = this.carService.getAllCars();
+  }
+
+  ngOnInit(){
+    this.getAllReviews();
+    this.getMyCars();
+  }
 }
